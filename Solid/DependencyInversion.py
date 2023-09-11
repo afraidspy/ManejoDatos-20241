bc20eaafe4f6bdfb79f2104cb13b3a586b6274d3
Jessica Santizo Galicia <js@ciencias.unam.mx>
2023-09-10 21:09:35 -0600

    Borrando carpetas repetidas

--- Solid/DependencyInversion.py
+++
@@ -1,7 +0,0 @@
-'''
-The Dependency Inversion Principle (DIP)
-“Abstractions should not depend on details.
-Details should depend on abstraction.
-High-level modules should not depend on low-level modules.
-Both should depend on abstractions”
-'''

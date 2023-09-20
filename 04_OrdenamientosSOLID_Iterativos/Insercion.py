c4a9f3e182f620810d13a0048be9ed13941bb5bc
Jessica Santizo Galicia <js@ciencias.unam.mx>
2023-09-19 22:04:43 -0600

    Se cambia nombre de carpeta

---
+++ 04_OrdenamientosSOLID_Iterativos/Insercion.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Jessica
+"""
+
+from OrdenableIterativoAbstract import OrdenableIterativoAbstractClass;
+
+class Insercion(OrdenableIterativoAbstractClass):
+    
+    def ordenar(self,elementos):
+      print(elementos)
+      print("Empezando el algoritmo..")
+
+      for i in range(1,len(elementos)):
+          aux = elementos[i]
+          no_ordenado = False
+          if (not no_ordenado):
+              for j in range(i-1, -1,-1):
+                  #print("\ni={}j={} ".format( i,j))
+                  if (elementos[j]>aux):
+                     no_ordenado = True
+                     # elementos[j+1] = elementos[j]
+                     # elementos[j] = aux
+                     # print(elementos)
+                     self.intercambiar(elementos, j, j+1)
+              print(j,"->",elementos)
